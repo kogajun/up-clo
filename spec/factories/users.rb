df4867ee4f65bccb0c17e42808e7267d87776234
kogajun <jannoir890118@yahoo.co.jp>
2021-10-28 00:02:01 +0900

    単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { 'あいうえお' }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password }
+    password_confirmation { password }
   end
 end
