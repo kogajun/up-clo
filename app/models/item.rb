6f241332d62c6d1225b60d5de7362736a232880a
kogajun <jannoir890118@yahoo.co.jp>
2021-10-28 11:42:51 +0900

    アソシエーション
    アソシエーション
    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  
+  validates :user,        null: false, foreign_key: true
+  validates :brand,       length: { maximum: 40 }
+  validates :item_name,   null: false, length: { maximum: 20 }
+  validates :color_id,    null: false, numericality: { other_than: 0, message: "can't be blank" }
+  validates :category_id, null: false, numericality: { other_than: 0, message: "can't be blank" }
+  validates :pattern_id,  null: false, numericality: { other_than: 0, message: "can't be blank" }
+  validates :season_id,   null: false, numericality: { other_than: 0, message: "can't be blank" }
+  validates :price,       null: false, numericality: { only_integer: true, greater_than: 0, less_than: 10_000_000 }
+  validates :size,        null: false
+  validates :scene,       length: { maximum: 20 }
+  validates :memo,        length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
   
   validates :nickname, presence: true, length: { maximum: 6 },
                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
