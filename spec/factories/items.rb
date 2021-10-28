2754fa2e7d1b0dcabbe61a7a3e3a548ab9b6c26c
kogajun <jannoir890118@yahoo.co.jp>
2021-10-29 06:50:27 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    brand        { 'USED' }
+    item_name    { 'カットソー' }
+    category_id  { '2' }
+    color_id     { '2' }
+    pattern_id   { '2' }
+    season_id    { '2'}
+    price        { '300' }
+    size         { 'M' }
+    scene        { 'お出かけ' }
+    memo         { 'メモ' }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/image.png'), filename: 'image.png')
+    end
   end
 end
