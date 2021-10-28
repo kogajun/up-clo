b7d63f299603507c88302f047b56ab676c1d16a7
kogajun <jannoir890118@yahoo.co.jp>
2021-10-28 11:31:29 +0900

    itemモデルを作成
    README修正（カラム位置、sizeの追加）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_060600) do
+ActiveRecord::Schema.define(version: 2021_10_28_020321) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "brand"
+    t.string "item_name", null: false
+    t.integer "color_id", null: false
+    t.integer "category_id", null: false
+    t.integer "pattern_id", null: false
+    t.integer "season_id", null: false
+    t.integer "price", null: false
+    t.string "size", null: false
+    t.string "scene"
+    t.text "memo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_10_27_060600) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
