4b8644fe446f7b85d49b4dfcd733d0d8f1dc81f0
kogajun <jannoir890118@yahoo.co.jp>
2021-10-28 12:15:52 +0900

    ルーティング
    ルーティングとヘッダーからのリンク
    コントローラーへcreateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
   def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
   end
 
   private
@@ -10,4 +23,9 @@ class ItemsController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  def item_params
+    params.require(:item).permit(:brand, :item_name, :color_id, :category_id, :color_id, :pattern_id, :season_id, :price, :size, :scene, :memo).merge(user_id: current_user.id)
+  end
+
 end
